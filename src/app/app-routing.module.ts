3be6f1d451dcad6051c97d4e109cfc010a04caa8
Kisyoka7 <kisyokav@gmail.com>
2023-07-04 10:22:17 +0300

    second commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { SignupComponent } from './AUTHENTICATION/signup/signup.component';
+import { LoginComponent } from './AUTHENTICATION/login/login.component';
+import { HomepageComponent } from './PAGES/homepage/homepage.component';
+import { DonationComponent } from './PAGES/donation/donation.component';
+import { AboutusComponent } from './PAGES/aboutus/aboutus.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'', component: HomepageComponent},
+  {path:'signup', component:SignupComponent},
+  {path: 'login', component:LoginComponent},
+  {path: 'donate', component: DonationComponent},
+  {path: 'about', component:AboutusComponent},
+ 
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './AUTHENTICATION/login/login.component';
+import { SignupComponent } from './AUTHENTICATION/signup/signup.component';
+import { HeaderComponent } from './FRAMES/header/header.component';
+import { FooterComponent } from './FRAMES/footer/footer.component';
+import { HomepageComponent } from './PAGES/homepage/homepage.component';
+import { DonationComponent } from './PAGES/donation/donation.component';
+import { AboutusComponent } from './PAGES/aboutus/aboutus.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    SignupComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomepageComponent,
+    DonationComponent,
+    AboutusComponent,
+    
+    
   ],
   imports: [
     BrowserModule,
